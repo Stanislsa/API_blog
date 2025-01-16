f5ffa3c2db2afb980d331a916ab2dce755719c77
Rakotoarinosy <fehizoroarinosy@gmail.com>
2025-01-17 00:56:23 +0300

    finished sing up

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,22 +1,30 @@
 
 import bcrypt
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, EmailStr
 from app.db import get_conn
 
 router = APIRouter(prefix="/auth")
 
 class SignUpReq(BaseModel):
-    email: str
+    email: EmailStr
     username: str
     password: str
 
 @router.post("/signup")
 def signup(sign_up_req: SignUpReq):
-    print(sign_up_req)
     hashed = bcrypt.hashpw(sign_up_req.password.encode("utf-8"), bcrypt.gensalt())
     with get_conn() as conn:
         with conn.cursor() as cursor:
+            record = cursor.execute(
+                "select * from users where email = %s or username = %s",
+                [sign_up_req.email, sign_up_req.username],
+            )
+            record = cursor.fetchone()
+             
+            if record:
+                raise HTTPException(status_code=400, detail="user aleready exists")
+            
             cursor.execute(
                 "insert into users (email, username, password) values (%s,%s,%s)", [sign_up_req.email, sign_up_req.username, hashed]
             )
